97de27db3367656fbb559708df8fa65a2a585a70
gm <89198080+picaresquepicaro@users.noreply.github.com>
2024-12-30 13:17:41 -0600

    Update pdf_parser.py

--- pdf_parser.py
+++ pdf_parser.py
@@ -3,7 +3,7 @@ import os
 
 
 directory = "/run/user/1000/gvfs/smb-share:server=10.99.40.84,share=hackathon%20data/Medical Emergency Response Reports/"
-local_copy = "/home/mvp/Documents/hackathon/repo/emergency_reports.txt"
+local_copy = ""
 
 for filename in os.listdir(directory):
     f = os.path.join(directory, filename)
